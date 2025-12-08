eb525fda6354f62f52eec56f185866dd33d20f02
Moshe Kamensky <kamensky.bgu@gmail.com>
2025-12-08 10:06:29 +0200

    regenerate talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,152 +1,232 @@
 var addressPoints = [
   [
-    "Higher-internal-covers;;Higher internal covers;;;;Toronto, Canada",
-    43.6534817,
-    -79.3839347
-  ],
-  [
-    "Galois-groups-in-model-theory;;Galois groups in model theory;;2008-01-01;;Waterloo, Canada",
-    43.4652699,
-    -80.5222961
-  ],
-  [
-    "Definable-groups-of-partial-automorphisms;;Definable groups of partial automorphisms;;2007-01-01;;Chicago, USA",
-    41.8755616,
-    -87.6244212
-  ],
-  [
-    "Categorical-aspects-of-internality;;Categorical aspects of internality;;2011-01-01;;Waterloo, Canada",
-    43.4652699,
-    -80.5222961
-  ],
-  [
-    "Towards-model-theory-for-homotopical-structures;;Towards model theory for homotopical structures;;;;Jerusalem, Israel",
-    31.7788472,
-    35.2257856
-  ],
-  [
-    "Computing-the-Galois-group-of-a-linear-differential-equation;;Computing the Galois group of a linear differential equation;;2004-01-01;;Be\u2019er-Sheva, Israel",
-    31.2457442,
-    34.7925181
-  ],
-  [
-    "Imaginaries-in-valued-fields-of-positive-characteristic;;Imaginaries in valued fields of positive characteristic;;2016-01-01;;New-York, USA",
-    40.7127281,
-    -74.0060152
-  ],
-  [
-    "Some-model-theory-of-rational-dynamics;;Some model theory of rational dynamics;;;;Jerusalem, Israel",
-    31.7788472,
-    35.2257856
-  ],
-  [
-    "Tannakian-categories-over-fields-with-operators;;Tannakian categories over fields with operators;;;;Canterbury, UK",
-    51.2800275,
-    1.0802533
-  ],
-  [
-    "Model-theory-of-fibre-functors;;Model theory of fibre functors;;2007-01-01;;University of Camerino, Italy",
-    43.1396331,
-    13.0672155
-  ],
-  [
-    "Differential-tensor-categories;;Differential tensor categories;;2009-01-01;;New-York, USA",
-    40.7127281,
-    -74.0060152
-  ],
-  [
-    "The-Tannakian-formalism-over-fields-with-operators;;The Tannakian formalism over fields with operators;;;;Toronto, Canada",
-    43.6534817,
-    -79.3839347
-  ],
-  [
-    "Galois-theory-of-differential-equations-over-general-fields-of-constants;;Galois theory of differential equations over general fields of constants;;2016-01-01;;Paris",
-    48.8534951,
-    2.3483915
-  ],
-  [
-    "Galois-groups-of-linear-difference-equations;;Galois groups of linear difference equations;;2006-01-01;;Israel",
-    30.8124247,
-    34.8594762
-  ],
-  [
-    "Tannakian-formalism-over-fields-with-operators;;Tannakian formalism over fields with operators;;2012-01-01;;Leeds, UK",
-    53.7974185,
-    -1.5437941
-  ],
-  [
-    "Picard-Vessiot-Structures;;Picard-Vessiot Structures;;2014-01-01;;New-York, USA",
-    40.7127281,
-    -74.0060152
-  ],
-  [
-    "Applications-of-definable-automorphism-groups;;Applications of definable automorphism groups;;2006-01-01;;Oxford, UK",
-    51.7520131,
-    -1.2578499
-  ],
-  [
-    "A-model-theoretic-approach-to-the-Tannakian-formalism;;A model theoretic approach to the Tannakian formalism;;2010-01-01;;Rehovot, Israel",
-    31.8952532,
-    34.8105616
-  ],
-  [
-    "Tannakian-Categories-after-Deligne;;Tannakian Categories (after Deligne);;2010-01-01;;New-York, USA",
-    40.7127281,
-    -74.0060152
-  ],
-  [
-    "Definable-groups-of-Automorphisms;;Definable groups of Automorphisms;;2006-01-01;;Oxford University, Oxford, UK",
-    51.755223,
-    -1.230104
-  ],
-  [
-    "Model-theory-of-differential-fibre-functors;;Model theory of differential fibre functors;;2007-01-01;;University of Leeds",
-    53.8057925,
-    -1.555111
-  ],
-  [
-    "Model-theory-of-the-Tannakian-formalism;;Model theory of the Tannakian formalism;;2008-01-01;;Maryland, USA",
-    39.5162401,
-    -76.9382069
-  ],
-  [
-    "Fields-with-free-operators-in-positive-characteristic;;Fields with free operators in positive characteristic;;;;Manchester, UK",
-    53.4794892,
-    -2.2451148
-  ],
-  [
-    "Internality-and-the-Tannakian-formalism;;Internality and the Tannakian formalism;;2007-01-01;;Manchester, UK",
-    53.4794892,
-    -2.2451148
-  ],
-  [
-    "The-formalism-of-generalised-operators-and-related-model-theoretic-questions;;The formalism of generalised operators and related model theoretic questions;;;;Wroc\u0142aw, Poland",
-    51.1263106,
-    16.9781963
-  ],
-  [
-    "Internality-to-algebraically-and-differentially-closed-fields;;Internality to algebraically and differentially closed fields;;2008-01-01;;Bern, Switzerland",
-    46.9484742,
-    7.4521749
-  ],
-  [
-    "Differential-tensor-categories-and-model-theory;;Differential tensor categories and model theory;;2010-01-01;;USA",
+    "Differential_tensor_categories-2011-01-01-UIC;;Differential tensor categories;;2011-01-01;;USA",
     39.7837304,
     -100.445882
   ],
   [
-    "Tensor-Categories-for-Fields-with-Operators;;Tensor Categories for Fields with Operators;;2011-01-01;;New-York, USA",
-    40.7127281,
-    -74.0060152
+    "Galois_theory_of_differential_equations_over_general_fields_of_constants-2016-01-01-Paris_7;;Galois theory of differential equations over general fields of constants;;2016-01-01;;Paris",
+    48.8534951,
+    2.3483915
   ],
   [
-    "Differential-Galois-theory;;Differential Galois theory;;2006-01-01;;Norwich, UK",
+    "Higher_internal_covers-2023-08-23-University_of_Waterloo;;Higher internal covers;;2023-08-23;;Waterloo, Canada",
+    43.4652699,
+    -80.5222961
+  ],
+  [
+    "Differential_Galois_theory-2006-01-01-University_of_East_Anglia;;Differential Galois theory;;2006-01-01;;Norwich, UK",
     52.6285576,
     1.2923954
   ],
   [
-    "Definable-groups-of-automorphisms (Case Conflict);;Definable groups of automorphisms;;2007-01-01;;Hamilton, Canada",
+    "Imaginaries_in_valued_fields_of_positive_characteristic-2016-01-01-CUNY;;Imaginaries in valued fields of positive characteristic;;2016-01-01;;New-York, USA",
+    40.7127281,
+    -74.0060152
+  ],
+  [
+    "Model_theory_of_the_Tannakian_formalism-2008-01-01-University_of_Maryland;;Model theory of the Tannakian formalism;;2008-01-01;;Maryland, USA",
+    39.5162401,
+    -76.9382069
+  ],
+  [
+    "Galois_groups_in_model_theory-2008-01-01-University_of_Waterloo;;Galois groups in model theory;;2008-01-01;;Waterloo, Canada",
+    43.4652699,
+    -80.5222961
+  ],
+  [
+    "Higher_internal_covers-2022-01-06-University_of_Haifa;;Higher internal covers;;2022-01-06;;Haifa, Israel",
+    32.8191218,
+    34.9983856
+  ],
+  [
+    "Tannakian_formalism_over_fields_with_operators-2012-01-01;;Tannakian formalism over fields with operators;;2012-01-01;;Leeds, UK",
+    53.7974185,
+    -1.5437941
+  ],
+  [
+    "Definable_groups_of_partial_automorphisms-2007-01-01-Notre_Dame_university;;Definable groups of partial automorphisms;;2007-01-01;;Notre Dame, USA",
+    41.7045677,
+    -86.2382203
+  ],
+  [
+    "Definable_groups_of_automorphisms-2007-01-01-McMaster_University;;Definable groups of automorphisms;;2007-01-01;;Hamilton, Canada",
     43.2560802,
     -79.8728583
+  ],
+  [
+    "Internality_and_the_Tannakian_formalism-2007-01-01-University_of_Manchester;;Internality and the Tannakian formalism;;2007-01-01;;Manchester, UK",
+    53.4794892,
+    -2.2451148
+  ],
+  [
+    "Tannakian_Categories_after_Deligne-2010-01-01-CUNY;;Tannakian Categories (after Deligne);;2010-01-01;;New-York, USA",
+    40.7127281,
+    -74.0060152
+  ],
+  [
+    "Higher_internal_covers-2024-06-12-Wrocaw_University;;Higher internal covers;;2024-06-12;;Wroc\u0142aw, Poland",
+    51.1263106,
+    16.9781963
+  ],
+  [
+    "The_Tannakian_formalism_over_fields_with_operators-2016-07-252016-07-29-The_Fields_Institute;;The Tannakian formalism over fields with operators;;2016-07-25/2016-07-29;;Toronto, Canada",
+    43.6534817,
+    -79.3839347
+  ],
+  [
+    "Tannakian_categories_over_fields_with_operators-2019-07-012019-07-05-School_of_Mathematics_Statistics_and_Actuarial_Science_of_the_University_of_Kent;;Tannakian categories over fields with operators;;2019-07-01/2019-07-05;;Canterbury, UK",
+    51.2800275,
+    1.0802533
+  ],
+  [
+    "The_formalism_of_generalised_operators_and_related_model_theoretic_questions-2019-10-05-Wrocaw_University;;The formalism of generalised operators and related model theoretic questions;;2019-10-05;;Wroc\u0142aw, Poland",
+    51.1263106,
+    16.9781963
+  ],
+  [
+    "Tensor_Categories_for_Fields_with_Operators-2012-01-01;;Tensor Categories for Fields with Operators;;2012-01-01;;Boston",
+    42.3588336,
+    -71.0578303
+  ],
+  [
+    "Differential_tensor_categories-2009-01-01-CUNY;;Differential tensor categories;;2009-01-01;;New-York, USA",
+    40.7127281,
+    -74.0060152
+  ],
+  [
+    "A_model_theoretic_approach_to_the_Tannakian_formalism-2010-01-01-Weizmann_Institute;;A model theoretic approach to the Tannakian formalism;;2010-01-01;;Rehovot, Israel",
+    31.8952532,
+    34.8105616
+  ],
+  [
+    "Computing_the_Galois_group_of_a_linear_differential_equation-2004-01-01-Ben-Gurion_University;;Computing the Galois group of a linear differential equation;;2004-01-01;;Be\u2019er-Sheva, Israel",
+    31.2457442,
+    34.7925181
+  ],
+  [
+    "Categorical_aspects_of_internality-2011-01-01;;Categorical aspects of internality;;2011-01-01;;Ol\u00e8ron, France",
+    45.9437695,
+    -1.3061227
+  ],
+  [
+    "Applications_of_definable_automorphism_groups-2006-01-01-Oxford_University;;Applications of definable automorphism groups;;2006-01-01;;Oxford, UK",
+    51.7520131,
+    -1.2578499
+  ],
+  [
+    "Tensor_Categories_for_Fields_with_Operators-2012-01-01-MPI;;Tensor Categories for Fields with Operators;;2012-01-01;;Bonn",
+    50.7352621,
+    7.1024635
+  ],
+  [
+    "Model_theory_of_the_Tannakian_formalism-2007-01-01-CIRM;;Model theory of the Tannakian formalism;;2007-01-01;;Luminy, France",
+    43.2326651,
+    5.4408365
+  ],
+  [
+    "Higher_internal_covers-2021-12-132021-12-17-The_Fields_Institute;;Higher internal covers;;2021-12-13/2021-12-17;;Toronto, Canada",
+    43.6534817,
+    -79.3839347
+  ],
+  [
+    "Picard-Vessiot_Structures-2014-01-01-CUNY;;Picard-Vessiot Structures;;2014-01-01;;New-York, USA",
+    40.7127281,
+    -74.0060152
+  ],
+  [
+    "Definable_groups_of_Automorphisms-2006-01-01;;Definable groups of Automorphisms;;2006-01-01;;Oxford University, Oxford, UK",
+    51.755223,
+    -1.230104
+  ],
+  [
+    "Applications_of_definable_automorphism_groups-2007-01-01-Mons;;Applications of definable automorphism groups;;2007-01-01;;Belgium",
+    50.6402809,
+    4.6667145
+  ],
+  [
+    "A_model_theoretic_approach_to_the_Tannakian_formalism-2010-01-01-University_of_Haifa;;A model theoretic approach to the Tannakian formalism;;2010-01-01;;Haifa, Israel",
+    32.8191218,
+    34.9983856
+  ],
+  [
+    "Model_theory_of_the_Tannakian_formalism-2009-01-01-University_of_Illinois;;Model theory of the Tannakian formalism;;2009-01-01;;Chicago, USA",
+    41.8755616,
+    -87.6244212
+  ],
+  [
+    "Applications_of_definable_automorphism_groups-2007-01-01-University_of_Leeds;;Applications of definable automorphism groups;;2007-01-01;;Leeds, UK",
+    53.7974185,
+    -1.5437941
+  ],
+  [
+    "Definable_groups_of_partial_automorphisms-2007-01-01-University_of_Illinois_Urbana-Champaign;;Definable groups of partial automorphisms;;2007-01-01;;Urbana-Champaign, USA",
+    40.1108179,
+    -88.1619746
+  ],
+  [
+    "Fields_with_free_operators_in_positive_characteristic-2018-08-032018-08-05-School_of_Mathematics__University_of_Manchester_UK;;Fields with free operators in positive characteristic;;2018-08-03/2018-08-05;;Manchester, UK",
+    53.4794892,
+    -2.2451148
+  ],
+  [
+    "Model_theory_of_fibre_functors-2007-01-01;;Model theory of fibre functors;;2007-01-01;;University of Camerino, Italy",
+    43.1396331,
+    13.0672155
+  ],
+  [
+    "Differential_tensor_categories_and_model_theory-2010-01-01-CUNY_Graduate_centre;;Differential tensor categories and model theory;;2010-01-01;;USA",
+    39.7837304,
+    -100.445882
+  ],
+  [
+    "Tensor_Categories_for_Fields_with_Operators-2011-01-01-CUNY;;Tensor Categories for Fields with Operators;;2011-01-01;;New-York, USA",
+    40.7127281,
+    -74.0060152
+  ],
+  [
+    "Categorical_aspects_of_internality-2011-01-01-University_of_Waterloo;;Categorical aspects of internality;;2011-01-01;;Waterloo, Canada",
+    43.4652699,
+    -80.5222961
+  ],
+  [
+    "Galois_groups_of_linear_difference_equations-2006-01-01;;Galois groups of linear difference equations;;2006-01-01;;Israel",
+    30.8124247,
+    34.8594762
+  ],
+  [
+    "Definable_groups_of_partial_automorphisms-2007-01-01-University_of_Illinois_Chicago;;Definable groups of partial automorphisms;;2007-01-01;;Chicago, USA",
+    41.8755616,
+    -87.6244212
+  ],
+  [
+    "Model_theory_of_the_Tannakian_formalism-2009-01-01-UIUC;;Model theory of the Tannakian formalism;;2009-01-01;;USA",
+    39.7837304,
+    -100.445882
+  ],
+  [
+    "Towards_model_theory_for_homotopical_structures-2025-07-01-Universidad_Nacional_de_Colombia;;Towards model theory for homotopical structures;;2025-07-01;;Bogota, Colombia",
+    4.6533817,
+    -74.0836331
+  ],
+  [
+    "Some_model_theory_of_rational_dynamics-2025-03-20-The_Hebrew_University;;Some model theory of rational dynamics;;2025-03-20;;Jerusalem, Israel",
+    31.7788472,
+    35.2257856
+  ],
+  [
+    "Towards_model_theory_for_homotopical_structures-2025-05-28-The_Hebrew_University;;Towards model theory for homotopical structures;;2025-05-28;;Jerusalem, Israel",
+    31.7788472,
+    35.2257856
+  ],
+  [
+    "Internality_to_algebraically_and_differentially_closed_fields-2008-01-01;;Internality to algebraically and differentially closed fields;;2008-01-01;;Bern, Switzerland",
+    46.9484742,
+    7.4521749
+  ],
+  [
+    "Model_theory_of_differential_fibre_functors-2007-01-01;;Model theory of differential fibre functors;;2007-01-01;;University of Leeds",
+    53.8057925,
+    -1.555111
   ]
 ];
